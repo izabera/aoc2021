003119572193ac3a8ecc627e7da791212776e8e2
izabera <izaberina@gmail.com>
2021-11-25 21:15:42 +0000

    remove examples

--- 22.cpp
+++
@@ -1,8 +0,0 @@
-#include "day.hpp"
-ret day::part1() {
-    std::string solution = "input = [ ";
-    for (auto line : input)
-        solution += "<" + line + "> ";
-    return solution + "]";
-}
-ret day::part2() { return "default"; }
--- 7.cpp
+++
@@ -1,7 +0,0 @@
-#include "day.hpp"
-ret day::part1() {
-    return "the solution to part 1 is a string";
-}
-ret day::part2() {
-    return 777;
-}
