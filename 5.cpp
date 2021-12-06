2b2e41e79886ec7192ebdf285d6826596d45cc1f
izabera <izaberina@gmail.com>
2021-12-06 08:58:45 +0000

    wip

--- 5.cpp
+++ 5.cpp
@@ -1,6 +1,5 @@
 #include "day.hpp"
 #include <thread>
-#include <execution>
 ret day::part1() { return "<default>"; }
 //ret day::part1() {
 //    int grid[size][size]{};
@@ -39,8 +38,8 @@ ret day::part2() {
 
     struct line { int x0, y0, x1, y1; };
     std::vector<line> lines;
-    std::vector<int> results(nthreads);
     int size = 0;
+
     for (auto l : input) {
         auto xy = split(l);
         auto a = split<int>(xy[0], ',');
@@ -50,23 +49,21 @@ ret day::part2() {
         lines.push_back(line{x0, y0, x1, y1});
     }
 
-    auto calc = [nthreads, lines, size, &results](int idx) {
+    std::vector<int> results(nthreads);
+    auto calc = [nthreads, lines, size, &results](auto idx) {
         uint64_t total = 0;
 
         // solve it for each row in this range
-        int nextpercent = 10;
-        for (auto y = idx * size/nthreads; y < (idx+1) * size/nthreads && y < size; y++) {
-            auto percent = (y - idx*size/nthreads) * 100 / (size/nthreads);
-            if (percent >= nextpercent) {
-                std::cout << "thread " << idx << " " << percent << "%\n";
-                nextpercent += 10;
-            }
+        auto ymin = idx * size/nthreads;
+        auto ymax = std::min((idx+1) * size/nthreads, size);
+
+        for (auto y = ymin; y < ymax; y++) {
             std::vector<uint16_t> current(size);
             for (auto [x0, y0, x1, y1] : lines) {
                 if (!((y0 <= y && y <= y1) || (y0 >= y && y >= y1)))
                     continue;
 
-                int dx = std::clamp(x1 - x0, -1, 1);
+                auto dx = std::clamp(x1 - x0, -1, 1);
 
                 if (y0 == y1) {
                     while (x0 != x1) {
@@ -81,11 +78,8 @@ ret day::part2() {
                     current[x0 + dx * std::abs(y - y0)]++;
             }
 
-            //total += std::accumulate(current.begin(), current.end(), 0, [](auto sum, auto i) { return sum + (i >= 2); });
-            //total += std::reduce(std::execution::par, current.begin(), current.end(),  [](auto sum, auto i){ return sum + (i >= 2); });
-            //std::for_each(current.begin(), current.end(), [&](auto i){ total += i >= 2; });
-            #pragma omp parallel
-            for (auto i : current) total += i >= 2;
+            for (auto i : current)
+                total += i >= 2;
         }
         results[idx] = total;
     };
